0be0013362ebda60a2b8cde270633aca1172ace9
Rami Martinez <40069890+rammartinez00@users.noreply.github.com>
2022-03-31 13:33:05 -0600

    part 1 done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,11 +3,26 @@ const asyncHandler = require("express-async-handler");
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
 
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
+// Log in
 router.post(
   "/",
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -33,6 +48,15 @@ router.post(
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
+});
+
+router.get("/", restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject(),
+    });
+  } else return res.json({});
 });
 
 module.exports = router;
