c7761bef27d005daab336ea5c719c735acc99046
Rami Martinez <40069890+rammartinez00@users.noreply.github.com>
2022-04-10 20:00:58 -0600

    review seed data

---
+++ backend/db/seeders/20220411015310-ReviewData.js
@@ -0,0 +1,384 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "SpotReviews",
+      [
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 6,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 6,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 6,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 7,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 8,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 8,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 8,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 9,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 9,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 9,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 10,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 10,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 10,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 11,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 11,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 11,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 12,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 12,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 12,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 13,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 13,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 13,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 1,
+          comment:
+            "This spot cured my anger issues... TEMPORARILY... I AM ANGRY!",
+          userId: 1,
+          spotId: 14,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 3,
+          comment: "This is a great spot! The toilet was not working.",
+          userId: 2,
+          spotId: 14,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          rating: 5,
+          comment: "This spot was amazing, loved every minute of it!",
+          userId: 3,
+          spotId: 14,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("SpotReviews", null, {});
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  },
+};
